5efba5c72ecd1a98cc151a36a71d64ad8ef15ed1
Robert Kaiser <paintballmanrob@gmail.com>
2023-08-08 21:25:49 -0400

    Added functionality for 5 day forecast

--- Assets/main.js
+++ Assets/main.js
@@ -1,137 +1,44 @@
-// GIVEN a weather dashboard with form inputs
-// WHEN I search for a city
-// THEN I am presented with current and future conditions for that city and that city is added to the search history
-// WHEN I view current weather conditions for that city
-// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed
-// WHEN I view future weather conditions for that city
-// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
-// WHEN I click on a city in the search history
-// THEN I am again presented with current and future conditions for that city
+// API Key for OpenWeatherMap
+const API_KEY = "7d03544563882cc0c835356bf14a646b";
 
-// 1 create a function to store search history
-// 2 create a function to display search history
-// 4. Create a current weather section
-// 5. Create a 5 day forecast section
-// 6. Create a function to get the weather data
-// 7. Create a function to display the weather data
-// 8. Create a function to save the search history
-// 9. Create a function to display the search history
-// 10. Create a function to display the current weather
-// 11. Create a function to display the 5 day forecast
-// 12. Create a function to display the humidity
-// 13. Create a function to display the wind speed
+// An async function to fetch weather data of a given city
+const fetchWeatherData = async (cityName) => {
+  // Request URL for the API
+  const requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=imperial`;
 
-// var requestURL = 'https://api.openweathermap.org/data/2.5/forecast?q=' + searchResult + '&appid=7d03544563882cc0c835356bf14a646b';
+  // Fetch data from the API
+  const response = await fetch(requestUrl);
 
+  // Throw error in case of unsuccessful request
+  if (!response.ok) {
+    throw new Error(response.statusText);
+  }
 
+  // Parsing the response to JSON format
+  const data = await response.json();
+  return data;
+};
 
+// An async function to fetch 5-day weather data based on given latitude and longitude
+const fetchWeatherData5Day = async (lat, lon) => {
+  let requestUrl2 = `https://api.openweathermap.org/data/2.5/forecast/?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=imperial`;
 
-
-// var searchResult = document.getElementById('button-addon2').addEventListener('click', (event) => {
-//     // Prevent the default form submission behavior
-//     event.preventDefault();
-
-//     // Get the value from the input field
-//     const query = document.getElementById('search-input').value;
-
-//     // add query to the local storage
-//     localStorage.setItem('city name', query);
-
-
-//     // Do something with the search query
-//     processQuery(query);
-
-// });
-
-// processQuery = (query) => {
-//     console.log("Search query:", query);
-//     createNewSearchLink(query);
-
-//     // Here you can process the query, e.g., call an API, filter results, etc.
-// }
-
-
-// createNewSearchLink = (query) => {
-//     // Create a new <a> element
-//     const newLink = $('<a>');
-
-//     // Set the href attribute
-//     newLink.attr('href', 
-//     'https://api.openweathermap.org/data/2.5/forecast?q=${query}&appid=7d03544563882cc0c835356bf14a646b');
-//     newLink.addClass('list-group-item list-group-item-action');
-
-//     // Set the text() content
-//     newLink.textContent = query;
-
-//     // Add the new element to the DOM
-//     $('past-searches').append(newLink);
-// }
-
-// capturePastSearches = () => {
-//     localStorage.setItem('Past Search Results', $('past-searches').text());
-// }
-
-// loadPastSearches = () => {
-//     $(document).ready(() => {
-//         let pastSearches = localStorage.getItem('Past Search Results');
-//         if (pastSearches) {
-//             $('past-searches').text(pastSearches);
-//         }
-//     })};
-
-
-fetchWeatherAPI = (searchResult) => {
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + searchResult + '&appid=7d03544563882cc0c835356bf14a646b' +
-    '&units=imperial')
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-    let humidity = data.main.humidity;
-    let windSpeed = data.wind.speed;
-    let temp = data.main.temp;
-    let date = new Date(data.dt * 1000).toDateString() + $('<br>') + data.weather[0].icon;
-
-    // document.getElementById('date-icon-main').textContent(date);
-    document.getElementById('temp-main').innerHTML = temp;
-    $('#humidity-main').text(humidity);
-    $('#wind-speed-main').text(windSpeed);
-
+  // Fetch data and update UI
+  fetch(requestUrl2)
+    .then((response) => {
+      return response.json();
     })
-    .catch(err => alert("Enter a valid city name!"));
-}
+    .then((data) => {
+      console.log(data);
 
-const searchResult = document.getElementById('button-addon2').addEventListener('click', (event) => {
-    // Prevent the default form submission behavior
-    event.preventDefault();
+      // Update UI for each day in the 5-day forecast
+      // Increment by 8 as the API provides 3-hourly data, and we want daily data
+      for (let i = 4, j = 1; j <= 5; i += 8, j++) {
+        updateDailyUI(j, data.list[i]);
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
 
-    // Get the value from the input field
-    let query = document.getElementById('search-input').value;
-
-    // // add query to the local storage
-    // let city = JSON.stringify(query);
-    // localStorage.setItem('city name', query);
-    // localStorage.getItem('city name').append(query);
-
-
-    fetchWeatherAPI(query);
-    console.log(query);
-    console.log(fetchWeatherAPI)
-    
-});
-
-// let cities = [];
-
-// let searchHistory = JSON.parse(localStorage.getItem("city")) || [];
-// for (let i = 0; i < searchHistory.length; i++) {
-//   getHistory(searchHistory[i]);
-// }
-
-// // Save the city searched in the local storage
-// const saveToLocalStorage = (city) => {
-//   cities = JSON.parse(localStorage.getItem("city")) || [];
-//   if (!cities.includes(city)) {
-//     cities.push(city);
-//     localStorage.setItem("city", JSON.stringify(cities));
-//     getHistory(city);
-//   }
-// };
