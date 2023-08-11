cddab260b0d3c5c3d4e5c929f55de4a53000fd17
Robert Kaiser <paintballmanrob@gmail.com>
2023-08-11 00:58:22 -0400

    Update main.js
    fixed local storage clear event

--- Assets/main.js
+++ Assets/main.js
@@ -211,6 +211,16 @@ window.onload = () => {
   }
 };
 
-window.onbeforeunload = () => {
-  localStorage.removeItem("searchHistory"); // Clear a specific item in local storage
+window.onbeforeunload = function (e) {
+    window.localStorage.unloadTime = JSON.stringify(new Date());
 };
+window.onload = function () {
+    let loadTime = new Date();
+    let unloadTime = new Date(JSON.parse(window.localStorage.unloadTime));
+    let refreshTime = loadTime.getTime() - unloadTime.getTime();
+    if(refreshTime>3000) //3000 milliseconds
+    {
+        localStorage.removeItem('searchHistory');
+    }
+};
+
